df6b261dfad760479915635f43bcfecb0e8f411d
Adarsh <adarsh.soni41@gmail.com>
2026-04-26 00:47:12 +0530

    fix api base url

--- src/api.js
+++ src/api.js
@@ -1,17 +1,36 @@
 import axios from "axios";
 
+const BASE_URL =
+  process.env.REACT_APP_API_URL ||
+  "https://virtual-office-backend-3e1e.onrender.com/api";
+
 const API = axios.create({
-  baseURL: "https://virtual-office-backend-3e1e.onrender.com/api",
+  baseURL: BASE_URL,
 });
 
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
+API.interceptors.request.use(
+  (req) => {
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
+    if (token) {
+      req.headers.Authorization = `Bearer ${token}`;
+    }
 
-  return req;
-});
+    return req;
+  },
+  (error) => Promise.reject(error),
+);
+
+API.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default API;
